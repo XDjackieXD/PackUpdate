20104023be009ed94f2950d6c6e984552629de86
Kilobyte22 <stiepen22@gmx.de>
2019-06-10 01:24:00 +0200

    Add disabled and optional flags

--- src/main/scala/at/chaosfield/packupdate/common/Component.scala
+++ src/main/scala/at/chaosfield/packupdate/common/Component.scala
@@ -26,17 +26,24 @@ class Component(val name: String, val version: String, val downloadUrl: Option[U
         )
         files.find(_.exists()) match {
           case Some(file) =>
-            hash match {
-              case Some(h) =>
-                val d = DigestUtils.sha256Hex(new FileInputStream(file))
-                //println(s"Hash of $name: $d <=> $h")
-                d == h
-              case _ =>
-                println(s"Warning: Could not validate integrity of Component $name, because no hash was provided")
-                true
+            val disabled = file.getName.endsWith(".disabled")
+            if (!flags.contains(ComponentFlag.Optional) && flags.contains(ComponentFlag.Disabled) != disabled) {
+              // The mod is in wrong disabled state and should be repaired
+              false
+            } else {
+              hash match {
+                case Some(h) =>
+                  val d = DigestUtils.sha256Hex(new FileInputStream(file))
+                  //println(s"Hash of $name: $d <=> $h")
+                  d == h
+                case _ =>
+                  println(s"Warning: Could not validate integrity of Component $name, because no hash was provided")
+                  true
+              }
             }
           case None => false // Even if no checksum is provided, we know something is wrong if the file is missing
         }
+
       case _ =>
         println(s"Warning: Checking integrity of component type ${componentType.stringValue} not supported yet, assuming working")
         true
@@ -49,6 +56,8 @@ class Component(val name: String, val version: String, val downloadUrl: Option[U
       case PackSide.Server => !flags.contains(ComponentFlag.ClientOnly)
     }
   }
+
+  def display = s"$name $version"
 }
 
 object Component {
